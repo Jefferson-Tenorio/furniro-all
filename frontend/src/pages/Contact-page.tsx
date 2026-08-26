bcaf1dc6cefff8f0c220fe4934aad6f6156a3f69
jefferson-tenorio <tenoriojefferson@proton.me>
2026-08-26 01:04:15 -0300

    feat(contact): add intro heading to contact page

--- frontend/src/pages/Contact-page.tsx
+++ frontend/src/pages/Contact-page.tsx
@@ -14,6 +14,17 @@ export default function ContactPage() {
   return (
     <>
       <PageBanner breadcrumbCurrent="Contact" breadcrumbHome="Home" title="Contact" />
+<div className="mx-auto mt-[98px] flex w-full max-w-[700px] flex-col items-center text-center">
+  <h1 className="font-poppins text-[36px] font-semibold leading-[100%] tracking-[0%] text-neutral-900">
+    Get in Touch with us
+  </h1>
+
+  <p className="font-poppins mt-[7px] text-[16px] font-normal leading-[100%] tracking-[0%] text-[#9F9F9F]">
+    For More Information About Our Product & Services. Please Feel Free To
+    Drop Us An Email. Our Staff Always Be There To Help You Out. Do Not
+    Hesitate!
+  </p>
+</div>
       <div
         className="mx-auto flex w-full max-w-6xl flex-col items-start gap-[30px]
                    px-6 mb-[123px] mt-[98px] lg:flex-row lg:justify-between"
